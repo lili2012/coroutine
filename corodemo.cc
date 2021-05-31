14c80e15e9b02c886d176c28815691ad8c1df3bb
lili2012 <github_2020@outlook.com>
2021-05-31 16:37:14 +0800

    slightly modify compile options and code to compile in vs2019

--- corodemo.cc
+++ corodemo.cc
@@ -4,43 +4,48 @@ clang++ -std=c++20 -stdlib=libc++ -fcoroutines-ts -Wall -Werror corodemo.cc
 */
 
 #include <concepts>
-#include <coroutine>
+/*#include <coroutine>*/
+#include<experimental/coroutine>
 #include <exception>
 #include <iostream>
-
+using namespace std;
+using namespace experimental;
 struct ReturnObject {
   struct promise_type {
     ReturnObject get_return_object() { return {}; }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() { return {}; }
+    suspend_never initial_suspend() { return {}; }
+    suspend_never final_suspend() { return {}; }
     void unhandled_exception() {}
+    void return_void() noexcept {}
   };
 };
 
 struct Awaiter {
-  std::coroutine_handle<> *hp_;
+  coroutine_handle<>* hp_;
   constexpr bool await_ready() const noexcept { return false; }
-  void await_suspend(std::coroutine_handle<> h) { *hp_ = h; }
+  void await_suspend(coroutine_handle<> h) { 
+    *hp_ = h; 
+  }
   constexpr void await_resume() const noexcept {}
 };
 
 ReturnObject
-counter(std::coroutine_handle<> *continuation_out)
+counter(coroutine_handle<>* continuation_out)
 {
-  Awaiter a{continuation_out};
+  Awaiter a{ continuation_out };
   for (unsigned i = 0;; ++i) {
     co_await a;
-    std::cout << "counter: " << i << std::endl;
+    cout << "counter: " << i << endl;
   }
 }
 
 void
 main1()
 {
-  std::coroutine_handle<> h;
+  coroutine_handle<> h;
   counter(&h);
   for (int i = 0; i < 3; ++i) {
-    std::cout << "In main1 function\n";
+    cout << "In main1 function\n";
     h();
   }
   h.destroy();
@@ -51,24 +56,25 @@ struct ReturnObject2 {
     ReturnObject2 get_return_object() {
       return {
         // Uses C++20 designated initializer
-        .h_ = std::coroutine_handle<promise_type>::from_promise(*this)
+        .h_ = coroutine_handle<promise_type>::from_promise(*this)
       };
     }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() { return {}; }
+    suspend_never initial_suspend() { return {}; }
+    suspend_never final_suspend() { return {}; }
     void unhandled_exception() {}
+    void return_void() noexcept {}
   };
 
-  std::coroutine_handle<promise_type> h_;
-  operator std::coroutine_handle<promise_type>() const { return h_; }
+  coroutine_handle<promise_type> h_;
+  operator coroutine_handle<promise_type>() const { return h_; }
 };
 
 ReturnObject2
 counter2()
 {
   for (unsigned i = 0;; ++i) {
-    co_await std::suspend_always{};
-    std::cout << "counter2: " << i << std::endl;
+    co_await suspend_always{};
+    cout << "counter2: " << i << endl;
   }
 }
 
@@ -76,9 +82,9 @@ void
 main2()
 {
   // A coroutine_handle<promise_type> converts to coroutine_handle<>
-  std::coroutine_handle<> h = counter2();
+  coroutine_handle<> h = counter2();
   for (int i = 0; i < 3; ++i) {
-    std::cout << "In main2 function\n";
+    cout << "In main2 function\n";
     h();
   }
   h.destroy();
@@ -89,28 +95,29 @@ struct ReturnObject3 {
     unsigned value_;
 
     ReturnObject3 get_return_object() {
-      return ReturnObject3 {
-        .h_ = std::coroutine_handle<promise_type>::from_promise(*this)
+      return ReturnObject3{
+        .h_ = coroutine_handle<promise_type>::from_promise(*this)
       };
     }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() { return {}; }
+    suspend_never initial_suspend() { return {}; }
+    suspend_never final_suspend() { return {}; }
     void unhandled_exception() {}
+    void return_void() noexcept {}
   };
 
-  std::coroutine_handle<promise_type> h_;
-  operator std::coroutine_handle<promise_type>() const { return h_; }
+  coroutine_handle<promise_type> h_;
+  operator coroutine_handle<promise_type>() const { return h_; }
 };
 
 template<typename PromiseType>
 struct GetPromise {
-  PromiseType *p_;
+  PromiseType* p_;
   bool await_ready() { return false; } // says yes call await_suspend
-  bool await_suspend(std::coroutine_handle<PromiseType> h) {
+  bool await_suspend(coroutine_handle<PromiseType> h) {
     p_ = &h.promise();
     return false;         // says no don't suspend coroutine after all
   }
-  PromiseType *await_resume() { return p_; }
+  PromiseType* await_resume() { return p_; }
 };
 
 ReturnObject3
@@ -120,17 +127,17 @@ counter3()
 
   for (unsigned i = 0;; ++i) {
     pp->value_ = i;
-    co_await std::suspend_always{};
+    co_await suspend_always{};
   }
 }
 
 void
 main3()
 {
-  std::coroutine_handle<ReturnObject3::promise_type> h = counter3();
-  ReturnObject3::promise_type &promise = h.promise();
+  coroutine_handle<ReturnObject3::promise_type> h = counter3();
+  ReturnObject3::promise_type& promise = h.promise();
   for (int i = 0; i < 3; ++i) {
-    std::cout << "counter3: " << promise.value_ << std::endl;
+    cout << "counter3: " << promise.value_ << endl;
     h();
   }
   h.destroy();
@@ -142,19 +149,20 @@ struct ReturnObject4 {
 
     ReturnObject4 get_return_object() {
       return {
-        .h_ = std::coroutine_handle<promise_type>::from_promise(*this)
+        .h_ = coroutine_handle<promise_type>::from_promise(*this)
       };
     }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() { return {}; }
+    suspend_never initial_suspend() { return {}; }
+    suspend_never final_suspend() { return {}; }
     void unhandled_exception() {}
-    std::suspend_always yield_value(unsigned value) {
+    void return_void() noexcept {}
+    suspend_always yield_value(unsigned value) {
       value_ = value;
       return {};
     }
   };
 
-  std::coroutine_handle<promise_type> h_;
+  coroutine_handle<promise_type> h_;
 };
 
 ReturnObject4
@@ -168,9 +176,9 @@ void
 main4()
 {
   auto h = counter4().h_;
-  auto &promise = h.promise();
+  auto& promise = h.promise();
   for (int i = 0; i < 3; ++i) {
-    std::cout << "counter4: " << promise.value_ << std::endl;
+    cout << "counter4: " << promise.value_ << endl;
     h();
   }
   h.destroy();
@@ -180,23 +188,23 @@ struct ReturnObject5 {
   struct promise_type {
     unsigned value_;
 
-    ~promise_type() { std::cout << "promise_type destroyed" << std::endl; }
+    ~promise_type() { cout << "promise_type destroyed" << endl; }
     ReturnObject5 get_return_object() {
       return {
-        .h_ = std::coroutine_handle<promise_type>::from_promise(*this)
+        .h_ = coroutine_handle<promise_type>::from_promise(*this)
       };
     }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_always final_suspend() { return {}; }
+    suspend_never initial_suspend() { return {}; }
+    suspend_always final_suspend() { return {}; }
     void unhandled_exception() {}
-    std::suspend_always yield_value(unsigned value) {
+    suspend_always yield_value(unsigned value) {
       value_ = value;
       return {};
     }
     void return_void() {}
   };
 
-  std::coroutine_handle<promise_type> h_;
+  coroutine_handle<promise_type> h_;
 };
 
 ReturnObject5
@@ -212,9 +220,9 @@ void
 main5()
 {
   auto h = counter5().h_;
-  auto &promise = h.promise();
+  auto& promise = h.promise();
   while (!h.done()) { // Do NOT use while(h) (which checks h non-NULL)
-    std::cout << "counter5: " << promise.value_ << std::endl;
+    cout << "counter5: " << promise.value_ << endl;
     h();
   }
   h.destroy();
@@ -223,21 +231,21 @@ main5()
 template<typename T>
 struct Generator {
   struct promise_type;
-  using handle_type = std::coroutine_handle<promise_type>;
+  using handle_type = coroutine_handle<promise_type>;
 
   struct promise_type {
     T value_;
-    std::exception_ptr exception_;
+    exception_ptr exception_;
 
     Generator get_return_object() {
       return Generator(handle_type::from_promise(*this));
     }
-    std::suspend_always initial_suspend() { return {}; }
-    std::suspend_always final_suspend() { return {}; }
-    void unhandled_exception() { exception_ = std::current_exception(); }
-    template<std::convertible_to<T> From> // C++20 concept
-    std::suspend_always yield_value(From &&from) {
-      value_ = std::forward<From>(from);
+    suspend_always initial_suspend() { return {}; }
+    suspend_always final_suspend() { return {}; }
+    void unhandled_exception() { exception_ = current_exception(); }
+    template<convertible_to<T> From> // C++20 concept
+    suspend_always yield_value(From&& from) {
+      value_ = forward<From>(from);
       return {};
     }
     void return_void() {}
@@ -254,7 +262,7 @@ struct Generator {
   T operator()() {
     fill();
     full_ = false;
-    return std::move(h_.promise().value_);
+    return move(h_.promise().value_);
   }
 
 private:
@@ -264,7 +272,7 @@ private:
     if (!full_) {
       h_();
       if (h_.promise().exception_)
-        std::rethrow_exception(h_.promise().exception_);
+        rethrow_exception(h_.promise().exception_);
       full_ = true;
     }
   }
@@ -282,7 +290,7 @@ main6()
 {
   auto gen = counter6();
   while (gen)
-    std::cout << "counter6: " << gen() << std::endl;
+    cout << "counter6: " << gen() << endl;
 }
 
 int
